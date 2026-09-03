d361686c30c4efeeb063bda5756d64368239b455
Agatha Nunes da Silva <agathanunes.contato@gmail.com>
2026-09-03 11:49:37 -0300

    commit

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -14,10 +14,17 @@ describe('App', () => {
     expect(app).toBeTruthy();
   });
 
-  it('should render title', async () => {
-    const fixture = TestBed.createComponent(App);
-    await fixture.whenStable();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, taskflow');
-  });
+  // it('should render title', async () => {
+  //   const fixture = TestBed.createComponent(App);
+  //   await fixture.whenStable();
+  //   const compiled = fixture.nativeElement as HTMLElement;
+  //   expect(compiled.querySelector('h1')?.textContent).toContain('Hello, taskflow');
+  // });
+});
+
+it('should render the router outlet', () => {
+  const fixture = TestBed.createComponent(App);
+  fixture.detectChanges();
+  const compiled = fixture.nativeElement as HTMLElement;
+  expect(compiled.querySelector('router-outlet')).toBeTruthy();
 });
